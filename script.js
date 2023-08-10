2a35588a3e0b6e4a356e3a737936ee3aec9f2fae
Dylan J Dombrowski <d0domb01@louisville.edu>
2023-08-10 13:59:05 -0400

    first commit. Basic functions are working. onto step 2.

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+function subtract(num1, num2) {
+  let difference = num1 - num2;
+  return difference;
+}
+
+function multiply(num1, num2) {
+  let product = num1 * num2;
+  return product;
+}
+
+function divide(num1, num2) {
+  let quotient = num1 / num2;
+  quotient = Math.round(quotient * 10000) / 10000;
+  return quotient;
+}
+
+divide(2, 3);
